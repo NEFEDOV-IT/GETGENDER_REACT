765f6cf8e28266375b9e650fdeffb1aa1dde77fa
NEFEDOV_IT <eliaz_one@mail.ru>
2022-03-17 13:35:09 +0300

    function components

--- src/App.jsx
+++ src/App.jsx
@@ -1,115 +1,70 @@
 import './App.css'
-import React from 'react'
+import React, { useState } from 'react'
 
 const URL = 'https://api.genderize.io'
-
-const STATE = {
-    COLOR: 'rgba(20,0,255,0.85)',
-    ERROR_NAME: 'Please enter more than 2 characters',
-}
 
 function getUrl(item, firstName) {
     return `${item}?name=${firstName}`
 }
 
 async function getGender(userName) {
-    const inputGender = document.querySelector('.gender__input')
-
-    if (!userName) return inputGender.classList.add('error')
-
-    if (inputGender.classList.contains('error')) {
-        inputGender.classList.remove('error')
-    }
-
-    const fullUrl = getUrl(URL, userName)
-    const response = await fetch(fullUrl)
-    const gender = await response.json()
-    return gender.gender
+    const response = await fetch(getUrl(URL, userName))
+    return await response.json()
 }
 
-class FormSend extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {value: ''};
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+function FormSend() {
+    const [name, setName] = useState('');
+    const [gender, setGender] = useState('');
+    const [user, setUser] = useState('')
+
+    function saveName(e) {
+        setName(e.target.value)
     }
 
-    handleChange(e) {
-        this.setState({value: e.target.value})
-    }
-
-    handleSubmit(e) {
-        this.props.sendValue(this.state.value)
+    async function saveGender(e) {
         e.preventDefault()
+        const result = await getGender(name)
+        if (!result.gender) return
+        setGender(result.gender)
+        setUser(result.name + ' is')
     }
-
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit} action="#" className="gender__form">
-                <input value={this.state.value} onChange={this.handleChange} placeholder="Enter your name..." type="text" className="gender__input"/>
-                <br/>
-                <button type="submit" className="gender__button">Send</button>
-            </form>
-        )
-    }
-}
-
-function AnswerGender(props) {
-    let gender = props.gender
-    let name = props.name
-    let stateStyle
-
-    if (gender === null) gender = 'unknown'
-
-    if (name.length < 2) {
-        name = '';
-        gender = STATE.ERROR_NAME;
-        stateStyle = <span style={{color: STATE.COLOR}}>{gender}</span>;
-        return <p>Gender: {name} {stateStyle}</p>
-    }
-
-    if (name.trim()) {
-        name += " is"
-    } else name = STATE.ERROR_NAME
-    stateStyle = <span className="gender__answer">{gender}</span>
 
     return (
-        <p>Gender: {name} {stateStyle}</p>
+        <div>
+            <form action="#" className="gender__form">
+                <input
+                    onChange={saveName}
+                    placeholder="Enter your name..."
+                    type="text"
+                    className='gender__input'
+                />
+                <br/>
+                <button
+                    onClick={saveGender}
+                    type="submit"
+                    className="gender__button">
+                    Send
+                </button>
+            </form>
+            <p>Gender: {user} <span className="gender__answer">{gender}</span></p>
+        </div>
     )
 }
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {name: '', gender: ''}
-        this.getValue = this.getValue.bind(this)
-    }
+function App() {
 
-    async getValue(value) {
-        const infoGender = await getGender(value)
-        this.setState({name: value})
-        this.setState({gender: infoGender})
-    }
+    return (
+        <div className="wrapper">
+            <header className="header">GET GENDER</header>
 
-    render() {
-        return (
-            <div className="wrapper">
-                <header className="header">GET GENDER</header>
+            <main className="main">
+                <h1>Find the gender from a name:</h1>
+                <FormSend/>
+            </main>
 
-                <main className="main">
-                    <h1>Find the gender from a name:</h1>
-                    <FormSend sendValue={this.getValue} />
-                    <AnswerGender
-                        name={this.state.name}
-                        gender={this.state.gender}
-                    />
-                </main>
-
-                <footer className="footer">NEFEDOV_IT</footer>
-            </div>
-        )
-    }
+            <footer className="footer">2022</footer>
+        </div>
+    )
 }
 
 export default App
